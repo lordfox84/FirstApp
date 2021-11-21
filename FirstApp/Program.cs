d24f7d16a25c4b1ddfd78d34b519d0aa8c64757a
Peter Kontúr <peter.kontur@gmail.com>
2021-11-21 18:57:59 +0100

    First draft

---
+++ FirstApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Zadej řetězec:");
+            string textRetezce = Console.ReadLine();
+            textRetezce = textRetezce.ToLower();
+            string hledanyText = "itnetwork";
+
+            Console.WriteLine(textRetezce.Contains(hledanyText));
+            Console.ReadKey();
+        }
+    }
+}
